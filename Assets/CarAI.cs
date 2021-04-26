e76f30d58a153975b2c8ceb2dd4af79636140187
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2021-04-26 10:50:03 -0400

    A bunch of work on the AI
    some stuff is just for testing

---
+++ Assets/CarAI.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class CarAI : MonoBehaviour
+{
+    private float speed = 0.09F;
+    private bool isOnRoad = true;
+    public GameObject cube;
+    private List<GameObject> roadParts = new List<GameObject>();
+
+    private IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        float i = 0;
+        float rate = 1.0F / time;
+        while (i < 1.0F)
+        {
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("Road"))
+        {
+            isOnRoad = true;
+            transform.rotation = collision.gameObject.transform.rotation;
+            if (!roadParts.Contains(collision.gameObject))
+            {
+                roadParts.Add(collision.gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Road"))
+        {
+            //Debug.Log("Stopped colliding with road");
+            roadParts.Remove(collision.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (roadParts.Count <= 0)
+        {
+            isOnRoad = false;
+        }
+        
+        if (isOnRoad)
+        {
+            GameObject GO = roadParts[roadParts.Count - 1];
+            Vector3 GOScale = GO.transform.localScale;
+
+            float GOScaleX = GOScale.x / 2;
+            float GOScaleX2 = -GOScale.x / 2;
+
+            float GOScaleZ = GOScale.z / 2;
+            float GOScaleZ2 = -GOScale.z / 2;
+
+            //if (transform.position.x == GO.transform.position.x/* + (GOScaleZ / 2)*/)
+            //{
+            //    Debug.Log("Testing");
+            //}
+            if (transform.position.x >= (GO.transform.position.x + GOScaleX))
+            {
+                //transform.Rotate(0, 90, 0);
+                Debug.Log("If 1");
+            }
+            else if (transform.position.x <= (GO.transform.position.x + GOScaleX2))
+            {
+                transform.Rotate(0, 90, 0);
+                Debug.Log("If 2");
+            }
+            else if (transform.position.z >= (GO.transform.position.z + GOScaleZ))
+            {
+                Debug.Log("If 3");
+            }
+            else if (transform.position.z <= (GO.transform.position.z + GOScaleZ2))
+            {
+                Debug.Log("If 4");
+            }
+        }
+        StartCoroutine(updatePos());
+    }
+
+    IEnumerator updatePos()
+    {
+        if (isOnRoad)
+        {
+            Vector3 pointB = cube.transform.position; // This is the end pos
+            Vector3 pointA = transform.position; // This is the start pos
+            yield return StartCoroutine(MoveObject(transform, pointA, pointB, speed)); // Moves the car from pointA to pointB
+        }
+    }
+}
---
+++ Assets/CarPathing.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarPathing : MonoBehaviour
+{
+    //private Transform[] pathPoints;
+    //public GameObject path;
+    public float movmentSpeed;
+    private float timer;
+    private static Vector3 currentPos;
+    //private int currentPoint;
+    private Vector3 startPos;
+    public GameObject movePoint;
+    private bool isOnRoad;
+    private List<GameObject> roadParts = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pathPoints = path.GetComponentsInChildren<Transform>();
+        CheckPoint();
+    }
+
+    void CheckPoint()
+    {
+        timer = 0;
+        //if (movePoint.transform.position.y > 0 || movePoint.transform.position.y < 0)
+        //{
+        currentPos = /*pathPoints[currentPoint]*/movePoint.transform.position;
+        //}
+        //else if (movePoint.transform.position.y == 0)
+        //{
+        //currentPos.x = movePoint.transform.position.x;
+        //currentPos.y = 0;
+        //currentPos.z = movePoint.transform.position.z;
+        //}
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (roadParts.Count <= 0)
+        {
+            isOnRoad = false;
+        }
+        else
+        {
+            isOnRoad = true;
+        }
+        if (isOnRoad)
+        {
+            timer += Time.deltaTime * movmentSpeed;
+            if (transform.position != currentPos)
+            {
+                transform.position = Vector3.Lerp(startPos, currentPos, timer);
+            }
+            else
+            {
+                //if (currentPoint < pathPoints.Length - 1)
+                //{
+                //currentPoint++;
+                CheckPoint();
+                //}
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Road"))
+        {
+            if (!roadParts.Contains(collision.gameObject))
+            {
+                roadParts.Add(collision.gameObject);
+            }
+            transform.rotation = collision.gameObject.transform.rotation;
+            //Debug.Log(collision.gameObject.transform.rotation);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Road"))
+        {
+            roadParts.Remove(collision.gameObject);
+        }
+    }
+}
