c5c46b3ecfe2d1d76c91cf2015ec279cd2a82cd7
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2021-05-12 10:39:14 -0400

    New Car AI (WORKING!!!)

---
+++ Assets/CarController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Transform path;
+    public float maxSteerAngle = 45.0F;
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+    public WheelCollider wheelBL;
+    public WheelCollider wheelBR;
+    public float maxMotorTorque = 80.0F;
+    public float maxBrakeTorque = 150;
+    public float currentSpeed;
+    public float maxSpeed = 100;
+    public Vector3 centerOfMass;
+    public bool isBreaking;
+    public Texture2D textureNormal;
+    public Texture2D textureBraking;
+    public Renderer carRenderer;
+
+    private List<Transform> nodes;
+    private int currentNode = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GetComponent<Rigidbody>().centerOfMass = centerOfMass;
+
+        Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 0; i < pathTransforms.Length; i++)
+        {
+            if (pathTransforms[i] != path.transform)
+            {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        ApplySteer();
+        Drive();
+        CheckWayPoint();
+        Braking();
+    }
+
+    private void ApplySteer()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+        relativeVector /= relativeVector.magnitude;
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+        wheelFL.steerAngle = newSteer;
+        wheelFR.steerAngle = newSteer;
+    }
+
+    private void Drive()
+    {
+        currentSpeed = 2 * Mathf.PI * wheelFL.radius * wheelFL.rpm * 60 / 1000;
+        if (currentSpeed < maxSpeed && !isBreaking)
+        {
+            wheelFL.motorTorque = maxMotorTorque;
+            wheelFR.motorTorque = maxMotorTorque;
+        }
+        else
+        {
+            wheelFL.motorTorque = 0;
+            wheelFR.motorTorque = 0;
+        }
+    }
+
+    private void CheckWayPoint()
+    {
+        if (Vector3.Distance(transform.position, nodes[currentNode].position) < 0.5F)
+        {
+            if (currentNode == nodes.Count - 1)
+            {
+                currentNode = 0;
+            }
+            else
+            {
+                currentNode++;
+            }
+        }
+    }
+
+    private void Braking()
+    {
+        if (isBreaking)
+        {
+            carRenderer.material.mainTexture = textureBraking;
+            wheelBL.brakeTorque = maxBrakeTorque;
+            wheelBR.brakeTorque = maxBrakeTorque;
+        }
+        else
+        {
+            carRenderer.material.mainTexture = textureNormal;
+            wheelBL.brakeTorque = 0;
+            wheelBR.brakeTorque = 0;
+        }
+    }
+}
